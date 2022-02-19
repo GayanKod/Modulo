725447d59c6d4c3026420253b2b5a5df4c340c3e
Dinindu Gunathilaka <dinindugunathilaka@gmail.com>
2022-02-19 16:49:36 +0530

    added the sample data for

---
+++ client/src/components/Timeline/data.tsx
@@ -0,0 +1,18 @@
+const events =[
+    {
+        event_name:"Commencement of the semester",
+        date: "25-01-2022",
+        category: {
+            tag: 'medium',
+            color: '#018f69'
+        },
+        link: {
+            url:
+                'https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2',
+            text: 'Read more'
+        }
+    },
+    {
+        // Another object with data
+    }
+];
