33b107c94368fa72da34bb8055e052b352109279
uvini917 <65717498+uvini917@users.noreply.github.com>
2022-03-04 09:11:07 +0530

    page structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/Landing";
-import LecHomePage from "./components/LecHomePage";
+import LecHomePage from "./pages/LecHomePage";
 
 function App() {
   return (
