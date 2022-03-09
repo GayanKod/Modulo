44dcc353c6985ddf3b7dce542176217abd75cfdb
Imalsha Liyanage <imalshashehaniliyanage@gmail.com>
2022-03-09 15:10:26 +0530

    Create notice input

---
+++ client/src/components/Noticeboard/NoticeInput.tsx
@@ -0,0 +1,110 @@
+import React, {FC, ChangeEvent, useState} from 'react';
+import {INotice} from "./interface";
+
+import "../../styles/NoticeInput.scss";
+import NoticeCard from './NoticeCard';
+
+
+const NoticeInput :FC = () => {
+
+    const [notice, setNotice] =useState<string>("");
+    const [description, setDescription] =useState<string>("");
+    const [noticeList, setNoticeList] =useState<INotice[]>([]);
+   
+  
+   
+    const handleChange =(event:ChangeEvent<HTMLInputElement> ) : void => {
+     
+     if(event.target.name==="notice"){
+       setNotice(event.target.value);
+     }
+     else {
+       setDescription(event.target.value);
+     }
+    
+    };
+   
+     const addNotice = (): void => {
+       const newNotice ={noticeTitle: notice, description: description };
+        setNoticeList([...noticeList, newNotice ]);
+        setNotice("");
+        setDescription("");
+     
+     };
+   
+     const deleteNotice =(noticeTitleToDelete: string): void =>
+      {
+         setNoticeList(noticeList.filter((notice)=>{
+          return notice.noticeTitle !== noticeTitleToDelete;
+   
+        }));
+     };
+
+     const editNotice =(noticeToEdit: string): void =>
+     {
+        setNoticeList(noticeList.filter((notice)=>{
+         return notice.noticeTitle !== noticeToEdit;
+  
+       }));
+    };
+
+    const sendSMS =(noticeToSendSMS: string): void =>
+    {
+       setNoticeList(noticeList.filter((notice)=>{
+        return notice.noticeTitle !== noticeToSendSMS;
+ 
+      }));
+   };
+  
+   
+     return  (
+       <div className="NoticeInput">
+        
+       <div className="header">
+         <br>
+         </br>
+         <div>
+            <p>Notice Title:</p>
+            <div className="inputContainer">
+            <input 
+            type="text" 
+            name="notice" 
+            value={notice}
+            onChange={handleChange}/>
+            </div>
+
+         </div>
+         
+        <br></br>
+
+        <div>
+            <p>Description:</p>
+            <div className="inputContainer">
+            <input 
+            type="text" 
+            name="description" 
+            value={description}
+            onChange={handleChange}/>
+    
+       
+         </div>
+
+        
+        </div>
+        <br></br>
+        
+   
+         <button onClick={addNotice}>+ Add Notice</button>
+         </div>
+         <div className="noticeList">
+          {noticeList.map((notice: INotice, key:number) => {
+         return <NoticeCard key={key} notice={notice} deleteNotice={deleteNotice} editNotice={editNotice} sendSMS={sendSMS}/>;
+       })}
+        </div>
+       </div>
+     );
+   };
+   
+
+
+export default NoticeInput;
---
+++ client/src/components/Noticeboard/interface.ts
@@ -0,0 +1,5 @@
+export interface INotice{
+    
+    noticeTitle: string;
+    description: string;
+}
