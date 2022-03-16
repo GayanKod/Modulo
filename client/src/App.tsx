ccd3e3e0c460051312be740f8696c4d4a798dbf6
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-03-16 15:17:07 +0530

    Create Admin Panel Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/Landing";
 import LecHomePage from "./pages/LecHomePage";
 import LoginPage from "./pages/Login";
+import AdminPanelDashboard from "./pages/AdminPanelDashboard";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/lec-hall-allocation" element={<LecHomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/admin-panel/dashboard" element={<AdminPanelDashboard />} />
         </Routes>
       </Router>
     </>
---
+++ client/src/pages/AdminPanelDashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavbarAP from '../components/AdminPanelComponents/NavbarAP';
+
+const AdminPanelDashboard = () => {
+  return (
+    <>
+        <NavbarAP />
+    </>
+  )
+}
+
+export default AdminPanelDashboard
