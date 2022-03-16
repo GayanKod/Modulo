2681795d93f92a724f4a72424db299a0826dee3e
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-03-16 16:26:01 +0530

    Create Admin Panel Sidebar and style it

--- client/src/pages/AdminPanelDashboard.tsx
+++ client/src/pages/AdminPanelDashboard.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import "../styles/AdminPanelDashboard.scss";
 import NavbarAP from '../components/AdminPanelComponents/NavbarAP';
+import SidebarAP from '../components/AdminPanelComponents/SidebarAP';
 
 const AdminPanelDashboard = () => {
   return (
     <>
         <NavbarAP />
+        <div className="sidebarAP-container">
+            <SidebarAP />
+            <div className="dashboardspace">dashboard space</div>
+        </div>
     </>
   )
 }
