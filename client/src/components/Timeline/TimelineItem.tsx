d5ec6dd58b813d65432e0e1b8a570f7f357325d0
Dinindu Gunathilaka <dinindugunathilaka@gmail.com>
2022-03-17 13:57:04 +0530

    Sass chnages

--- client/src/components/Timeline/TimelineItem.tsx
+++ client/src/components/Timeline/TimelineItem.tsx
@@ -2,16 +2,11 @@ import "../../styles/Timeline.scss";
 const TimelineItem = ({data}:{[key:string]:any}) => (
     <div className="timeline-item">
         <div className="timeline-item-content">
-
-            {/* <span className="tag" style={{ background: data.category.color }}>
-                {data.category.tag}
-            </span> */}
-            
             <div className="event">
                         <h2 className="event-name" >
                             {data.eventName}
                         </h2>
-                        <div className="Date">
+                        <div className="date">
                             <time className="event-starting-date">{data.startingDate}</time>
                              {data.endingDate && 
                              <time className="event-starting-date"> - {data.startingDate}</time>}
