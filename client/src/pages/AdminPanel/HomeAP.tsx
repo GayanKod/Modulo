481a2b328807fffe60c45edd8359f4d12274def8
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-04-02 07:52:17 +0530

    Create Admin Panel Home as a Page

--- client/src/pages/AdminPanel/HomeAP.tsx
+++ client/src/pages/AdminPanel/HomeAP.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import "../../styles/HomeAP.scss";
 import FeaturedInfo from "../../components/AdminPanelComponents/FeaturedInfo";
+import SmallWidget from "../../components/AdminPanelComponents/SmallWidget";
+import LargeWidget from "../../components/AdminPanelComponents/LargeWidget";
 
 const HomeAP = () => {
   return (
     <div className="homeAP-container">
         <FeaturedInfo />
+        <div className="home-widgets">
+            <SmallWidget />
+            <LargeWidget />
+        </div>
     </div>
   )
 }
