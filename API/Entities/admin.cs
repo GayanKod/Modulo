86e56de772c585f7b519236707aa371790eec315
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-05-17 21:58:18 +0530

    AuthnRegBE - Create admin model

---
+++ API/Entities/admin.cs
@@ -0,0 +1,15 @@
+namespace API.Entities
+{
+    public class admin
+    {
+        public String Admin_ID { get; set; }
+        public String Name { get; set; }
+        public DateOnly DOB { get; set; }
+        public String Gender { get; set; }
+        public String StreetNo { get; set; }
+        public String Street { get; set; }
+        public String Town { get; set; }
+        public String AdminType { get; set; }
+
+    }
+}
