a48800aabafbb11b2c668b20ed35067e2ac2c501
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-05-22 22:30:31 +0530

    AuthnRegBE - Create Admin Controller GET Method

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Admin>>> Get()
+        {
+            var admins = new List<Admin>
+            {
+                new Admin
+                {
+                    AdminID = "A001",
+                    AdminName ="Gayan",
+                    DOB="1999/5/9",
+                    Gender ="Male",
+                    StreetNo="3/13",
+                    Street="Rathu Palliya Rd",
+                    Town="Panadura",
+                    AdminType="SuperAd"
+                }
+            };
+            return Ok(admins);
+        }
+    }
+}
