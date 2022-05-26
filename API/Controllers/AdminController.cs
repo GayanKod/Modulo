ac1fd68ab9a833839cde0740234ef8b3c3a30936
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-05-26 18:49:50 +0530

    AuthnRegBE - Complete CRUD in AdminController

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -8,11 +8,8 @@ namespace API.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<List<Admin>>> Get()
+        private static List<Admin> admins = new List<Admin>
         {
-            var admins = new List<Admin>
-            {
                 new Admin
                 {
                     AdminID = "A001",
@@ -22,10 +19,72 @@ namespace API.Controllers
                     StreetNo="3/13",
                     Street="Rathu Palliya Rd",
                     Town="Panadura",
-                    AdminType="SuperAd"
+                    AdminType="SA"
+                },
+                new Admin
+                {
+                    AdminID = "A002",
+                    AdminName ="Dinidu",
+                    DOB="1999/12/10",
+                    Gender ="Male",
+                    StreetNo="89",
+                    Street="Galle Rd",
+                    Town="Colombo",
+                    AdminType="SA"
                 }
-            };
+        };
+
+        [HttpGet]
+        public async Task<ActionResult<List<Admin>>> GetAdmins()
+        {
             return Ok(admins);
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Admin>> GetAdmin(String id)
+        {
+            var admin = admins.Find(ad => ad.AdminID == id);
+            if(admin == null)
+                return BadRequest("Admin Not Found!");
+            return Ok(admin);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Admin>>> AddAdmin(Admin admin)
+        {
+            admins.Add(admin);
+            return Ok(admins);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Admin>>> UpdateAdmin(Admin req)
+        {
+            var admin = admins.Find(ad => ad.AdminID == req.AdminID);
+            if (admin == null)
+                return BadRequest("Admin Not Found!");
+            
+            admin.AdminID = req.AdminID;
+            admin.AdminName = req.AdminName;
+            admin.Gender = req.Gender;
+            admin.DOB = req.DOB;
+            admin.StreetNo = req.StreetNo;
+            admin.Street = req.Street;
+            admin.Town = req.Town;
+            admin.AdminType = req.AdminType;
+
+            return Ok(admins);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Admin>>> DeleteAdmin(String id)
+        {
+            var admin = admins.Find(ad => ad.AdminID == id);
+            if (admin == null)
+                return BadRequest("Admin Not Found!");
+
+            admins.Remove(admin);
+            return Ok(admins);
+        }
+
     }
 }
