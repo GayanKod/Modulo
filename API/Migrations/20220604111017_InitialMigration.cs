725c0d264e974325d0e67b3d8b5ba6546f52a801
Binuli <binulifernando@gmail.com>
2022-06-08 10:25:16 +0530

    Created Model,controller & migration files for Document. Connected SQLServer using .NET6. Created RESt APIs (GET/PUT/POST/DELETE)

---
+++ API/Migrations/20220604111017_InitialMigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Documents",
+                columns: table => new
+                {
+                    DocumentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DocumentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DocumentType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalPages = table.Column<int>(type: "int", nullable: false),
+                    DocumentSize = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DocumentURL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documents", x => x.DocumentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Documents");
+        }
+    }
+}
---
+++ API/Models/Document.cs
@@ -0,0 +1,16 @@
+﻿namespace API
+{
+    public class Document
+    {
+        public int DocumentId { get; set; }
+        public string DocumentName { get; set; } = string.Empty;
+        public string DocumentType { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty ;
+        public int TotalPages { get; set; }
+        public int DocumentSize { get; set; }
+        public DateTime Date { get; set; }
+        public string DocumentURL { get; set; } = string.Empty ;
+        public int UserId { get; set; } 
+        
+    }
+}
