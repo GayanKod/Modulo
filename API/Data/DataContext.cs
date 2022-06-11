9db8ba7414aac16d98e10244cbf77cf2409a4895
Imalsha Liyanage <imalshashehaniliyanage@gmail.com>
2022-06-11 23:20:43 +0530

    notice - backend - minor changes to DataContext

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext : : DbContext
+    public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options): base(options){}
 
