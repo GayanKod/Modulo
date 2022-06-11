7c4cb66733b0486ed5048b3f71d8d424b064a9f2
Imalsha Liyanage <imalshashehaniliyanage@gmail.com>
2022-06-11 23:12:51 +0530

    notice - backend - created NoticeController

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,6 +7,8 @@ namespace API.Data
 {
     public class DataContext : : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options): base(options){}
+
+        public DbSet<Notice> Notices{ get; set; }
     }
 }
