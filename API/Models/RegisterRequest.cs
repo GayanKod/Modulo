ccea15154f80c17ed8108770fb8b56a61735d5fc
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-06-12 05:11:21 +0530

    AuthnRegBE - Create RegisterRequest Model

---
+++ API/Models/RegisterRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Models
+{
+    public class RegisterRequest
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required, MinLength(6, ErrorMessage = "Please enter at least 6 characters")]
+        public string Password { get; set; } = string.Empty;
+        [Required, Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+    }
+}
