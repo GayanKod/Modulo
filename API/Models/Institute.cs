af57160ef470fd860d9c422db73ff9c92baa9e38
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-06-12 22:36:41 +0530

    AuthnRegBE - Create Institute Model

---
+++ API/Models/Institute.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class Institute
+    {
+        public string InstituteID { get; set; }
+        public string InstituteName { get; set; } = string.Empty;
+
+        [DataType(DataType.PhoneNumber)]
+        public string ContactNumber { get; set; } = string.Empty;
+    }
+}
