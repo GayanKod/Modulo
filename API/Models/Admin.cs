d305a0c1fb6ba9e656548d3fc678ef3bacf13fea
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-06-12 16:53:24 +0530

    AuthnRegBE - Make adminName, Gender, AdminType default value empty

--- API/Models/Admin.cs
+++ API/Models/Admin.cs
@@ -2,15 +2,15 @@
 {
     public class Admin
     {
-        public string AdminID { get; set; }
-        public string AdminName { get; set; }
+        public string AdminID { get; set; } 
+        public string AdminName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public DateTime DOB { get; set; }
-        public string Gender { get; set; }
+        public string Gender { get; set; } = string.Empty;
         public string? StreetNo { get; set; }
         public string? Street { get; set; }
         public string? Town { get; set; }
-        public string AdminType { get; set; }
+        public string AdminType { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = new byte[32];
         public byte[] PasswordSalt { get; set; } = new byte[32];
         public string? VerificationToken { get; set; }
