997365eecda354664af4c9f5da559cf765be1a09
Dinindu Gunathilaka <dinindugunathilaka@gmail.com>
2022-06-13 16:11:52 +0530

    timeline - added timeline form component

--- client/src/pages/Timeline.tsx
+++ client/src/pages/Timeline.tsx
@@ -2,12 +2,16 @@ import TimelineContainer from "../components/Timeline/TimelineContainer";
 import Navbar2 from "../components/Navbar2";
 import PageTitle from "../components/PageTitle";
 import "../styles/TimelinePage.scss";
+import Footer from "../components/Footer/Footer"
+import TimelineForm from "../components/Timeline/TimelineForm"
 function Timeline(){
     return(
         <>
         <Navbar2/>
         <PageTitle title="Activity Timeline"/>
         <TimelineContainer/>
+        <TimelineForm/>
+        <Footer/>
         </>
         
     );
