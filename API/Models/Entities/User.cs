a82e2cf1093cf62659f9a34b1e2820833bde0e69
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-06-20 23:09:11 +0530

    BE - remove JsonIgnore for getting Institute

--- API/Models/Entities/User.cs
+++ API/Models/Entities/User.cs
@@ -28,8 +28,6 @@ namespace API.Models.Entities
         public DateTime? VerifiedAt { get; set; }
         public string? PasswordResetToken { get; set; }
         public DateTime? ResetTokenExpires { get; set; }
-        
-        [JsonIgnore]
         public List<Institute> Institutes { get; set; }
 
     }
