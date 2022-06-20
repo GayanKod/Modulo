5dcc142132b66ab0a31e2f9e3641baef4552e1f8
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-06-20 06:11:07 +0530

    BE - Update User, Institute Tables

--- API/Models/Entities/User.cs
+++ API/Models/Entities/User.cs
@@ -1,28 +1,35 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace API.Models.Entities
 {
-    public class Admin
+    public class User
     {
         public int Id { get; set; }
-        public string AdminName { get; set; } = string.Empty;
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
         public string Email { get; set; } = string.Empty;
         public DateTime DOB { get; set; }
         public string Gender { get; set; } = string.Empty;
-        public string? StreetNo { get; set; }
+        public string? HomeNo { get; set; }
         public string? Street { get; set; }
         public string? Town { get; set; }
 
         [DataType(DataType.PhoneNumber)]
         public string MobileNumber { get; set; }
-        public string AdminType { get; set; } = string.Empty;
+        [Required]
+        public string Role { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = new byte[32];
         public byte[] PasswordSalt { get; set; } = new byte[32];
         public string? VerificationToken { get; set; }
         public DateTime? VerifiedAt { get; set; }
         public string? PasswordResetToken { get; set; }
         public DateTime? ResetTokenExpires { get; set; }
-
+        
+        [JsonIgnore]
         public List<Institute> Institutes { get; set; }
 
     }
