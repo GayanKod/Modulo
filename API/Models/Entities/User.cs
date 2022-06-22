a9fe62cb07d3f0023b4a9158835e1070111f966c
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-06-22 22:18:30 +0530

    AdminPanelBE - Add Validation to the User Entity

--- API/Models/Entities/User.cs
+++ API/Models/Entities/User.cs
@@ -10,7 +10,7 @@ namespace API.Models.Entities
         public string FirstName { get; set; } = string.Empty;
         [Required]
         public string LastName { get; set; } = string.Empty;
-        [Required]
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
         public DateTime DOB { get; set; }
         public string Gender { get; set; } = string.Empty;
