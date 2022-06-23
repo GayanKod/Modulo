c8b8d4a8fa442ae24f2477ea9707ac68f45100a3
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-06-23 23:25:37 +0530

    AdminPanelFE - Admin Panel Home Page Enhanced and Connect Frontend

--- client/src/components/AdminPanelComponents/FeaturedInfo.tsx
+++ client/src/components/AdminPanelComponents/FeaturedInfo.tsx
@@ -1,31 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import {isAuth} from "../../helpers/auth";
+import axios from 'axios';
 import "../../styles/FeaturedInfo.scss";
 
 const FeaturedInfo = () => {
+
+  const [aCount, setACount] = useState();
+  const [saCount, setSaCount] = useState();
+  const [seCount, setSeCount] = useState();
+  const [eCount, setECount] = useState();
+  const [sCount, setSCount] = useState();
+  const [uCount, setUCount] = useState();
+
+  useEffect(() => {
+    function getAdmins(){
+        axios.get(`https://localhost:5000/api/Stat/get-users-count/${isAuth().institutes[0].id}`).then((res) => {
+            setACount(res.data.adminsCount);
+            setSaCount(res.data.superAdminsCount);
+            setSeCount(res.data.superEditorsCount);
+            setECount(res.data.editorsCount);
+            setSCount(res.data.subscribersCount);
+            setUCount(res.data.adminsCount+res.data.superAdminsCount+res.data.superEditorsCount+res.data.editorsCount + res.data.subscribersCount + res.data.subscribersCount)
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }
+    getAdmins();
+  }, [])
+
   return (
+    <>
     <div className="featuredAP">
       <div className="featuredAP-Item">
         <span className="featuredAP-Title">Super Admins</span>
         <div className="featuredAP-CountContainer">
-          <span className="featuredAP-Count">02</span>
+          <span className="featuredAP-Count">{saCount}</span>
         </div>
-        <span className="featuredAP-Des">Total registered super admins</span>
+        <span className="featuredAP-Des">Total no. of registered Super Admins</span>
+      </div>
+      <div className="featuredAP-Item">
+        <span className="featuredAP-Title">Admins</span>
+        <div className="featuredAP-CountContainer">
+          <span className="featuredAP-Count">{aCount}</span>
+        </div>
+        <span className="featuredAP-Des">Total no. of registered Admins</span>
+      </div>
+      <div className="featuredAP-Item">
+        <span className="featuredAP-Title">Super Editors</span>
+        <div className="featuredAP-CountContainer">
+          <span className="featuredAP-Count">{seCount}</span>
+        </div>
+        <span className="featuredAP-Des">Total no. of registered Super Editors</span>
+      </div>
+      <div className="featuredAP-Item">
+        <span className="featuredAP-Title">Editors</span>
+        <div className="featuredAP-CountContainer">
+          <span className="featuredAP-Count">{eCount}</span>
+        </div>
+        <span className="featuredAP-Des">Total no. of registered Editors</span>
+      </div>
+    </div>
+    <div className="featuredAP">
+      <div className="featuredAP-Item">
+        <span className="featuredAP-Title">Subscribers</span>
+        <div className="featuredAP-CountContainer">
+          <span className="featuredAP-Count">{sCount}</span>
+        </div>
+        <span className="featuredAP-Des">Total no. of registered Subscribers</span>
       </div>
       <div className="featuredAP-Item">
         <span className="featuredAP-Title">Degrees</span>
         <div className="featuredAP-CountContainer">
-          <span className="featuredAP-Count">03</span>
+          <span className="featuredAP-Count">Degreee</span>
         </div>
-        <span className="featuredAP-Des">Total degrees offer</span>
+        <span className="featuredAP-Des">Total no. of Degrees offer</span>
       </div>
       <div className="featuredAP-Item">
-        <span className="featuredAP-Title">Subscribers</span>
+        <span className="featuredAP-Title">Users</span>
+        <div className="featuredAP-CountContainer">
+          <span className="featuredAP-Count">{uCount}</span>
+        </div>
+        <span className="featuredAP-Des">Total no. of registered users</span>
+      </div>
+      <div className="featuredAP-Item">
+        <span className="featuredAP-Title">Editors</span>
         <div className="featuredAP-CountContainer">
           <span className="featuredAP-Count">2,225</span>
         </div>
-        <span className="featuredAP-Des">Total no. of registered students</span>
+        <span className="featuredAP-Des">Total no. of registered Editors</span>
       </div>
     </div>
+  </>
   )
 }
 
