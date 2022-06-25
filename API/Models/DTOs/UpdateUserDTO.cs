1f613bd5b90a73e74e94b3a00c68847b8024998f
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-06-26 03:18:50 +0530

    AdminPanelBE - Create UpdateUserDTO

---
+++ API/Models/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.DTOs
+{
+    public class UpdateUserDTO
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string Gender { get; set; }
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Role { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string MobileNumber { get; set; }
+        public string HomeNo { get; set; }
+        public string Street { get; set; }
+        public string Town { get; set; }
+        //public int InstituteId { get; set; }
+    }
+}
