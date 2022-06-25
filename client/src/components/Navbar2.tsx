c72a3e9fe98ed4a6b2cf60036cec8fdc8b9196f2
uvini917 <65717498+uvini917@users.noreply.github.com>
2022-06-25 21:55:14 +0530

    added logo to navbar2

--- client/src/components/Navbar2.tsx
+++ client/src/components/Navbar2.tsx
@@ -1,8 +1,12 @@
+import LogoModulo from "../assets/img/LogoModuloLight.png";
+
 export default function Navbar2() {
   return (
     <div className="nav">
       <h2 className="logo">
-        <a href="/">modulo</a>
+        <a href="/">
+          <img src={LogoModulo} alt="logo" height={"50px"} />
+        </a>
       </h2>
       <h3 className="user">
         <a href="#">user </a>
