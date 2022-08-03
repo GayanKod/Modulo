6d5fe43fc83c760209fe0e2124f0c6bdd5a2fc1e
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-08-03 23:02:22 +0530

    AdminPanelBE - Add Degree count and Batch count to StatDTO

--- API/Models/DTOs/StatDTO.cs
+++ API/Models/DTOs/StatDTO.cs
@@ -7,6 +7,8 @@
         public int SuperEditorsCount { get; set; }
         public int EditorsCount { get; set; }
         public int SubscribersCount { get; set; }
+        public int DegreesCount { get; set; }
+        public int BatchesCount { get; set; }
 
     }
 }
