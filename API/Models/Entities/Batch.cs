c2c42dd6e8a97ed5df92730f0c0b0ebf2f571234
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-08-03 23:00:51 +0530

    AdminPanelBE - Create Batch Entity

---
+++ API/Models/Entities/Batch.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace API.Models.Entities
+{
+    public class Batch
+    {
+        public int Id { get; set; }
+        public string BatchName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        [JsonIgnore]
+        public List<User> Users { get; set; }
+        [JsonIgnore]
+        public Institute Institute { get; set; }
+        public int InstituteId { get; set; }
+    }
+}
