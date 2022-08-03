27aa93937bd957bb8e28ae2256a48e18aff2a26e
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-08-03 23:03:23 +0530

    AdminPanelBE - Create Add Degree DTO

---
+++ API/Models/DTOs/AddDegreeDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.DTOs
+{
+    public class AddDegreeDTO
+    {
+        [Required]
+        public string DegreeName { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public int DurationInYears { get; set; }
+        public int InstituteId { get; set; }
+    }
+}
