78d3bb93bbb7b5c35722ce79bc4f5d9a4fdd2406
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-08-03 21:37:28 +0530

    AdminPanelFE - Create and finalise batches pages

---
+++ client/src/pages/AdminPanel/Batches/APBatchesPage.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "../../../styles/AdminPanelHome.scss";
+import NavbarAP from '../../../components/AdminPanelComponents/NavbarAP';
+import SidebarAP from '../../../components/AdminPanelComponents/SidebarAP';
+import LecHallList from './BatchesList';
+
+const APLecHallsPage = () => {
+  return (
+    <>
+        <NavbarAP />
+        <div className="sidebarAP-container">
+            <SidebarAP />
+            <div className="AP-AdminsTitle">
+            </div>
+            <LecHallList  />
+        </div>
+    </>
+  )
+}
+
+export default APLecHallsPage;
