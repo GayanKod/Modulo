a8ac7e4b2a0b5b29ead7cdcfa713bf2225ff9847
Gayan Kodithuwakku <gayan.lakshan9@gmail.com>
2022-08-03 23:03:37 +0530

    AdminPanelBE - Create Add Batch DTO

---
+++ API/Models/DTOs/AddBatchDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models.DTOs
+{
+    public class AddBatchDTO
+    {
+        public string BatchName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int InstituteId { get; set; }
+    }
+}
